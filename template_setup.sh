25fe925161e5e0092b31776621be4905656ad489
jennylsmith <18451714+jennylsmith@users.noreply.github.com>
2025-06-20 19:09:13 -0700

    wait for the actions run to complete, check run status, and pull the changes

--- template_setup.sh
+++ template_setup.sh
@@ -4,8 +4,16 @@ set -eou
 
 # name of the current repository
 REPO=$(git config --get remote.origin.url | sed -E "s/http.+com\/(.+).git/\1/")
+Echo "Setting up $REPO"
 
 # Trigger the repository_dispatch event
-gh api repos/$REPO/dispatches -f "event_type=template-generated"
+gh api repos/$REPO/dispatches -f "event_type=template-generated" 
 
-gh run list
+# wait for the test to run
+wait 10
+
+# list the gh actions run
+gh run list --status success --json status
+
+# pull the changes from the remote repository
+git pull 
